ec746da1742e02c681c1455df8f9352e538cfba8
Desiree Quade <nekodez@gmail.com>
2022-03-30 16:36:22 -0500

    completed question 4

--- Program.cs
+++ Program.cs
@@ -5,24 +5,17 @@ Console.WriteLine("--- 1. Practice creating arrays ---");
 
 // Example: Array of animals
 string[] animalArray = {"fish", "cat", "bird", "dog"};
-Console.WriteLine("Animals are:");
-foreach (string i in animalArray)
-{
-    Console.WriteLine(i);
-}
-// 1.a. TODO: Make an array with some favorite foods
+Console.WriteLine("Animals are: " + String.Join(", ", animalArray));
 
+// 1.a. TODO: Make an array with some favorite foods
 string[] favoriteFoods = {"raspberries", "strawberries", "pizza", "cheesecake"};
 
 // console.log('My favorite foods are: ' + favoriteFoods);
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar
 //      to the example above
-Console.WriteLine("Favorite foods are:");
-foreach (string food in favoriteFoods)
-{
-    Console.WriteLine(food);
-}
+Console.WriteLine("Favorite foods are: " + String.Join(", ", favoriteFoods));
+
 
 // 2. Array.length - tells you how many items are in the array
 Console.WriteLine("--- 2. Length of an array ---");
@@ -47,19 +40,20 @@ Console.WriteLine("Last animal is " + animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
-Console.WriteLine(animalArray[animalArray.Length-1]);
+Console.WriteLine("Last animal is " + animalArray[animalArray.Length-1]);
 
 // 4. Adding & Removing Array Items
-// console.log('--- 4. Adding and removing array items ---');
+Console.WriteLine("--- 4. Adding and removing array items ---");
 
 // Example: Add an animal to the end using Array.push
 // animalArray.push('penguin');
 // console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
+Array.Resize(ref favoriteFoods, favoriteFoods.Length + 1);
+favoriteFoods[favoriteFoods.GetUpperBound(0)] = "blueberries";
 
-// favoriteFoods.push('blueberries');
-// console.log('Added a favorite food to end, ',favoriteFoods);
+Console.WriteLine("new favorite foods: " + string.Join(", ", favoriteFoods));
 
 // Example: Remove the last animal by using Array.pop
 // let removedAnimal = animalArray.pop();
@@ -68,19 +62,22 @@ Console.WriteLine(animalArray[animalArray.Length-1]);
 
 // 4.b. TODO: Remove the food at the end of your array &
 //      log both the food removed and the updated array
+string lastFood = favoriteFoods[4];
+Console.WriteLine("removed food: " + lastFood);
 
-// let removedFood=favoriteFoods.pop();
-// console.log('Removed the last favorite food', removedFood);
-// console.log('My favorite foods are now', favoriteFoods);
+Array.Resize(ref favoriteFoods, favoriteFoods.Length - 1);
+Console.WriteLine("new favorite foods: " + string.Join(", ", favoriteFoods));
 
 // Example: Add an animal to the beginning using Array.unshift
 // animalArray.unshift('walrus');
 // console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
+string[] newFoods = new string[favoriteFoods.Length + 1];
+newFoods[0] = "mango";
+Array.Copy(favoriteFoods, 0, newFoods, 1, favoriteFoods.Length);
 
-// favoriteFoods.unshift('mangos');
-// console.log(`Added a favorite food to beginning: ${favoriteFoods}`);
+Console.WriteLine("new foods: " + string.Join(", ", newFoods));
 
 // Example: Remove the first animal using Array.shift
 // removedAnimal = animalArray.shift();
@@ -89,7 +86,7 @@ Console.WriteLine(animalArray[animalArray.Length-1]);
 
 // 4.d TODO: Remove the food at the beginning of your array &
 //     log both the food removed and the updated array
-
-// removedFood=favoriteFoods.shift();
-// console.log('Removed the first favorite food', removedFood);
-// console.log('My favorite foods are now', favoriteFoods);
+List<string> list = new List<string>(newFoods);
+        list.RemoveAt(0);
+        newFoods = list.ToArray();
+Console.WriteLine("original fav foods: " + String.Join(", ", newFoods));
